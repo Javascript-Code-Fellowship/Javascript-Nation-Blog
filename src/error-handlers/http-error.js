5a13de45a5c41c1bb69eb5dabcc7be5c928a5f69
Jaya DeHart <dehartj97@gmail.com>
2021-08-26 17:51:05 -0700

    added some comments

--- src/error-handlers/http-error.js
+++ src/error-handlers/http-error.js
@@ -1,3 +1,6 @@
+//this class basically just extends the error class and adds one extra property, status code
+//so to call one of these you go: return next(new HttpError("message",404)); or whatever code obviously
+
 class HttpError extends Error {
     constructor(message, statusCode) {
         super(message);
